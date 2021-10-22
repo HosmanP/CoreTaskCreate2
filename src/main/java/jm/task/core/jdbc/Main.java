376090fab76e2a5434acfb9eef9345f44f9f7231
HosmanP <hosman@mail.ru>
2021-10-22 17:51:07 +0300

    Все сделано! Исправлено согласно комментариям

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -11,15 +11,15 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<User> users = new ArrayList<>();
-        UserServiceImpl user = new UserServiceImpl();
-        user.createUsersTable();
-        user.saveUser("Иван", "Иванов", (byte) 12);
-        user.saveUser("Петр", "Щеглов", (byte) 44);
-        user.saveUser("Лея", "Петровна", (byte) 23);
-        user.saveUser("Остап", "Валиев", (byte) 12);
-        users = user.getAllUsers();
+        UserServiceImpl userServiceImpl = new UserServiceImpl();
+        userServiceImpl.createUsersTable();
+        userServiceImpl.saveUser("Иван", "Иванов", (byte) 12);
+        userServiceImpl.saveUser("Петр", "Щеглов", (byte) 44);
+        userServiceImpl.saveUser("Лея", "Петровна", (byte) 23);
+        userServiceImpl.saveUser("Остап", "Валиев", (byte) 12);
+        users = userServiceImpl.getAllUsers();
         System.out.println(users);
-        user.cleanUsersTable();
-        user.dropUsersTable();
+        userServiceImpl.cleanUsersTable();
+        userServiceImpl.dropUsersTable();
     }
 }
